8893bc52d070d984e927cfbb04e4074f496e76e8
gabrielS0ares <soaresgabriel33@gmail.com>
2024-04-11 20:06:57 -0300

    Feat: configuracao tema e fonts

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,22 @@
 export const defaultTheme = {
-    primary: "purple",
-    secondary: "orange",
-    white: "#fff"
+    white: '#fff',
+
+    'zinc-100' : '#f4f4f5',
+    'zinc-200' : '#e4e4e7',
+    'zinc-300' : '#d4d4d8',
+    'zinc-400' : '#a1a1aa',
+    'zinc-500' : '#71717a',
+    'zinc-600' : '#52525b',
+    'zinc-700' : '#3f3f46',
+    'zinc-800' : '#27272a',
+    'zinc-900' : '#18181b',
+
+    'emerald-300' : '#6ee7b7',
+    'emerald-500' : '#10b981',
+    'emerald-700' : '#047857',
+
+    'rose-500,' : '#f43f5e',
+    'rose-700' : '#be123c',
+
+    'amber' : '#f59e0b'
 }
