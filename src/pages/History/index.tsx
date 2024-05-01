35e88084738d00513a2623c9107705c0cc02943d
gabrielS0ares <soaresgabriel33@gmail.com>
2024-04-30 22:03:50 -0300

    Feat: Páginas e rotas - Página: History

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,54 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export default function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Uma tarefa</td>
+              <td>20 min</td>
+              <td>há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa</td>
+              <td>20 min</td>
+              <td>há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa</td>
+              <td>20 min</td>
+              <td>há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa</td>
+              <td>20 min</td>
+              <td>há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa</td>
+              <td>20 min</td>
+              <td>há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
